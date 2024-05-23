3832c6963dff1e5da1ba4f1029dfff13fc19a09e
kanchansharmaa <120258056+kanchansharmaa@users.noreply.github.com>
2024-05-23 11:29:19 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,72 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var getCallback=require('./routes/callback/callback.route')
+var getSubDoi =require('./routes/SubDOI/SubDOI.router')
+var sendSMS=require('./routes/SMS/Sms.route')
+var sendBillingRequest=require('./routes/billing/billing.routes')
+var callbacks=require('./routes/newcallback/newcallback.routes')
+var check =require('./routes/Check')
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+
+    ],
+  })
+);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/images', express.static('images'));
+ 
+// app.use('/', indexRouter);
+
+
+app.use('/users', usersRouter);
+app.use('/', getCallback)
+app.use('/subscribe', getSubDoi)
+app.use('/sendSMS',sendSMS )
+app.use('/sendbillingrequest',sendBillingRequest)
+app.use('/callback-notify', callbacks)
+
+
+
+
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
